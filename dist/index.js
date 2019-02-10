6d60d57503a5230ebd35ffdc07ba2c342e0c95df
meidan.zemer <meidan@gmail.com>
2019-02-10 11:09:08 +0200

    change module

--- dist/index.js
+++ dist/index.js
@@ -1,18 +1,12 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var moment_1 = __importDefault(require("moment"));
-function getTimeDate(ts) {
-    var now = moment_1.default();
-    var momentTs = moment_1.default(ts);
-    if (moment_1.default.duration(now.diff(momentTs)) < moment_1.default.duration(60 * 60 * 24 * 1000)) {
+import moment from 'moment';
+export function getTimeDate(ts) {
+    const now = moment();
+    const momentTs = moment(ts);
+    if (moment.duration(now.diff(momentTs)) < moment.duration(60 * 60 * 24 * 1000)) {
         return momentTs.fromNow();
     }
     return momentTs.format('MMMM Do YYYY');
 }
-exports.getTimeDate = getTimeDate;
-exports.contactPointsCollectionName = 'contactPoints';
-exports.discussionsSubCollectionName = 'contactPointDiscussions';
-exports.messagesSubCollectionName = 'contactPointDiscussionMessages';
+export const contactPointsCollectionName = 'contactPoints';
+export const discussionsSubCollectionName = 'contactPointDiscussions';
+export const messagesSubCollectionName = 'contactPointDiscussionMessages';
