0fb553d03cdc51f5ce82148a3bfec4b55c574a72
meidan.zemer <meidan@gmail.com>
2019-02-14 16:30:04 +0200

    fix typescript module configuration to match firebase function

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,18 @@
-import moment from 'moment';
-export function getTimeDate(ts) {
-    const now = moment();
-    const momentTs = moment(ts);
-    if (moment.duration(now.diff(momentTs)) < moment.duration(60 * 60 * 24 * 1000)) {
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const moment_1 = __importDefault(require("moment"));
+function getTimeDate(ts) {
+    const now = moment_1.default();
+    const momentTs = moment_1.default(ts);
+    if (moment_1.default.duration(now.diff(momentTs)) < moment_1.default.duration(60 * 60 * 24 * 1000)) {
         return momentTs.fromNow();
     }
     return momentTs.format('MMMM Do YYYY H:m');
 }
-export const contactPointsCollectionName = 'contactPoints';
-export const discussionsSubCollectionName = 'contactPointDiscussions';
-export const messagesSubCollectionName = 'contactPointDiscussionMessages';
+exports.getTimeDate = getTimeDate;
+exports.contactPointsCollectionName = 'contactPoints';
+exports.discussionsSubCollectionName = 'contactPointDiscussions';
+exports.messagesSubCollectionName = 'contactPointDiscussionMessages';
